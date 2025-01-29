ec9e0c501e27f9fcec9072fa482c50fa752bcc80
Cole Murray <murray1236@students.d211.org>
2025-01-29 08:19:25 -0600

    New update

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -7,16 +7,65 @@ import {
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import {useState} from "react";
 
 export default function Index() {
+  const [selectedId, setSelectedId] = useState<string>("1");
+
+  type dataType = {
+    id: string; // unique identifier
+    title: string; // text we see in flatlist
+
+  }
+
+  const DATA: dataType[] = [
+    {id: '1', title: 'First'},
+    {id: '2', title: 'Second'},
+    {id: '3', title: 'Third'},
+    {id: '4', title: 'Fourth'},
+    {id: '5', title: 'Fifth'}
+];
+
+
+/*
+declaring a function called selectedList that recieves a paramerter
+of type dataType that we will refer to as 'item' I can access the values
+of item using dot notation
+*/
+
+const selectedList = (item: dataType) => {
+  setSelectedId(item.id);
+  console.log("selected " + item.title);
+}
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
-        <Text style={defaultStyles.title}>Insert Title Here</Text>
+        <Text style={defaultStyles.title}>Cole's FlatList</Text>
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList
+            data={DATA}
+            keyExtractor={(item: dataType) => item.id}
+            renderItem={({item}) => (
+              <TouchableOpacity onPress={() => selectedList(item)}>
+                <View style={[styles.titleContainer,
+                  {
+                    backgroundColor: 
+                      item.id === selectedId ? colors.primary : colors.secondary
+                  }
+                ]}>
+                  <Text style={[styles.titleText,
+                    {
+                      color:
+                        item.id === selectedId ? colors.text.light : colors.text.dark
+                    }
+                  ]}>{item.title}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
       </View>
     </View>
@@ -28,10 +77,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   titleContainer: {
-    marginTop: 5,
+    marginTop: 10,
     width: 300,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
+    borderRadius: 10,
+    backgroundColor: "steelblue",
   },
   titleText: {
     fontSize: 24,
